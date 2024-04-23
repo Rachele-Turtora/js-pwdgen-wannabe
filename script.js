33c0b63a6fb9d2d425776a413722d4459a76f7b2
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-23 15:12:31 +0200

    [UPD] esperimenti

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 "use strict"
 
+let currentElement = document.getElementById("password");
+let currentText = currentElement.innerHTML;
+
 // Taking data from the user
 const userName = prompt("Inserisci il tuo nome");
 const userLastName = prompt("Inserisci il tuo cognome");
 const userColor = prompt("Inserisci il tuo colore preferito");
+const maxNum = prompt("Inserisci un numero massimo");
 
-// Generating a random integer between 1 and 100
-let number = Math.floor(Math.random() * 100) + 1;
+// Generating a random integer between 1 and maxNum
+let number = Math.floor(Math.random() * maxNum) + 1;
 
-let password = `${userName} ${userLastName} ${userColor} ${number}`;
+// Generating password
+const password = `${userName} ${userLastName} ${userColor} ${number}`;
 
 // Printing password in document
-document.getElementById("password").innerHTML = password;
+currentText = `${currentText} ${password}`;
+currentElement.innerHTML = currentText;
 
 // Printing password in console
 console.log(password);
