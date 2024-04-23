7e1f4ae0c52a670887da04978ab796a56700f96b
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-23 15:15:11 +0200

    [FIX] password

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const maxNum = prompt("Inserisci un numero massimo");
 let number = Math.floor(Math.random() * maxNum) + 1;
 
 // Generating password
-const password = `${userName} ${userLastName} ${userColor} ${number}`;
+const password = `${userName}${userLastName}${userColor}${number}`;
 
 // Printing password in document
 currentText = `${currentText} ${password}`;
