fcd49ce3823c073883968c43e0e25c215a80b831
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-23 14:27:59 +0200

    [UPD] bonus 2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 "use strict"
 
 // Taking data from the user
-const userName = prompt("Inserisci il tuo nome")
-const userLastName = prompt("Inserisci il tuo cognome")
-const userColor = prompt("Inserisci il tuo colore preferito")
+const userName = prompt("Inserisci il tuo nome");
+const userLastName = prompt("Inserisci il tuo cognome");
+const userColor = prompt("Inserisci il tuo colore preferito");
 
-let password = `${userName} ${userLastName} ${userColor} 21`
+// Generating a random integer between 1 and 100
+let number = Math.floor(Math.random() * 100) + 1;
+
+let password = `${userName} ${userLastName} ${userColor} ${number}`;
 
 // Printing password in document
-document.getElementById("password").innerHTML = password
+document.getElementById("password").innerHTML = password;
 
 // Printing password in console
-console.log(password)
+console.log(password);
