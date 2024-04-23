fe6f61a46970b6d190f990a456e0fd4d12341ffb
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-23 14:18:08 +0200

    [UPD] generated password

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict"
 
+// Taking data from the user
+const userName = prompt("Inserisci il tuo nome")
+const userLastName = prompt("Inserisci il tuo cognome")
+const userColor = prompt("Inserisci il tuo colore preferito")
+
+let password = `${userName} ${userLastName} ${userColor} 21`
+
+// Printing password in document
+document.getElementById("password").innerHTML = password
+
