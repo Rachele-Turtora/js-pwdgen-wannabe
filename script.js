ede6b39c80c21256e81b801b81b370bd4dabb66f
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-24 13:28:42 +0200

    [FIX] proofreading

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const userColor = prompt("Inserisci il tuo colore preferito");
 const maxNum = prompt("Inserisci un numero massimo");
 
 // Generating a random integer between 1 and maxNum
-let number = Math.floor(Math.random() * maxNum) + 1;
+const number = Math.floor(Math.random() * maxNum) + 1;
 
 // Generating password
-const password = `${userName}${userLastName}${userColor}${number}`;
+const password = userName + userLastName + userColor + number;
 
 // Printing password in document
 currentText = `${currentText} ${password}`;
